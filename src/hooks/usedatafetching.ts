b6b93431df1d16480285cf2c24f90bb153473cdd
codeashdev <ahmadshoaibt@hotmail.com>
2023-10-23 09:47:51 +0300

    adds error catch to the response

--- src/hooks/usedatafetching.ts
+++ src/hooks/usedatafetching.ts
@@ -17,26 +17,32 @@ export const useDataFetching = () => {
 
   useEffect(() => {
     fetch("/items.json")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((data: { data: string[] }) => {
         const items = data.data.map((name, index) => ({
           id: index + 1,
           name: name,
         }));
         setJsonData(items);
-
+  
         const sessionSelectedItems = JSON.parse(localStorage.getItem('selectedItems') || '[]');
         setSelectedItems(sessionSelectedItems);
-        
+  
         setUnselectedItems(
           items.filter((item) => !sessionSelectedItems.some((selectedItem: Item) => selectedItem.id === item.id))
         );
         setFilteredItems(
           items.filter((item) => !sessionSelectedItems.some((selectedItem: Item) => selectedItem.id === item.id))
-        )
+        );
       })
-      .catch((error) => console.error('Error fetching data:', error));
+      .catch((error) => console.error('Error:', error));
   }, [setFilteredItems, setJsonData, setSelectedItems, setUnselectedItems]);
+  
 
   return { jsonData, selectedItems, unselectedItems, setSelectedItems, setUnselectedItems }
 };
